5d845ec7348cd692eaa49b23f744337301d392a4
Otu Udo <119851008+Otumiky@users.noreply.github.com>
2024-12-15 21:41:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
 resource "aws_security_group" "webserver" {
-  name        = "${var.cluster_name}-web-sg"
-  description = "Allow HTTP traffic"
-  vpc_id      = var.vpc_id
+  name        = "${var.cluster_name}-sg"
+  description = "Allow HTTP and SSH traffic for webserver"
 
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
     protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -18,35 +24,49 @@ resource "aws_security_group" "webserver" {
   }
 }
 
-resource "aws_launch_configuration" "webserver" {
-  name          = "${var.cluster_name}-lc"
-  image_id      = var.ami
-  instance_type = var.instance_type
-  security_groups = [
-    aws_security_group.webserver.id,
-  ]
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "${var.server_text}" > /var/www/html/index.html
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-  EOF
-}
-
-resource "aws_autoscaling_group" "webserver" {
-  name                 = "${var.cluster_name}-asg"
-  launch_configuration = aws_launch_configuration.webserver.id
-  min_size             = var.min_size
-  max_size             = var.max_size
-  desired_capacity     = var.desired_capacity
-  vpc_zone_identifier  = var.subnet_ids
-
-  lifecycle {
-    create_before_destroy = true
+data "terraform_remote_state" "db" {
+  backend = "s3"
+  config = {
+    bucket = var.db_remote_state_bucket
+    key    = var.db_remote_state_key
+    region = var.aws_region
   }
 }
 
-output "webserver_autoscaling_group_name" {
-  value = aws_autoscaling_group.webserver.name
+resource "aws_launch_template" "webserver" {
+  name_prefix   = "${var.cluster_name}-lt"
+  image_id      = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  user_data = base64encode(templatefile("${path.module}/user-data.sh", {
+    server_text = var.server_text
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  }))
+
+  network_interfaces {
+    security_groups = [aws_security_group.webserver.id]
+  }
+}
+
+resource "aws_autoscaling_group" "webserver" {
+  desired_capacity     = var.min_size
+  max_size             = var.max_size
+  min_size             = var.min_size
+  launch_template      = { id = aws_launch_template.webserver.id }
+  vpc_zone_identifier  = var.subnet_ids
+  health_check_type    = "EC2"
+  health_check_grace_period = 300
+  tags = [
+    {
+      key                 = "Name"
+      value               = var.cluster_name
+      propagate_at_launch = true
+    }
+  ]
+}
+
+output "webserver_sg_id" {
+  value = aws_security_group.webserver.id
 }
