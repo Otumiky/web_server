a9ca910721be8feb68173da7660e0504715875e4
Otu Udo <119851008+Otumiky@users.noreply.github.com>
2024-12-17 10:43:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,52 +1,28 @@
-# Reference the remote state for database configuration
-data "terraform_remote_state" "db" {
-  backend = "s3"
+resource "aws_instance" "example" {
+  ami                         = "ami-0b0ea68c435eb488d"
+  instance_type               = var.instance_type
+  vpc_security_group_ids      = [aws_security_group.instance.id]
+  user_data                   = <<-EOF
+                #!/bin/bash
+                   echo "Hello, World" > index.html
+                   nohup busybox httpd -f -p ${var.server_port} &
+                   EOF
+  user_data_replace_on_change = true
 
-  config = {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = var.aws_region
-  }
-}
-
-# Launch Configuration for Webserver Instances
-resource "aws_launch_configuration" "webserver" {
-  name_prefix   = "${var.cluster_name}-lc-"
-  image_id      = var.ami_id
-  instance_type = var.instance_type
-  user_data     = var.user_data
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Auto Scaling Group for Webserver Cluster
-resource "aws_autoscaling_group" "webserver_cluster" {
-  count                = var.enable_autoscaling ? 1 : 0
-  name                 = var.cluster_name
-  min_size             = var.min_size
-  max_size             = var.max_size
-  desired_capacity     = var.min_size
-  launch_configuration = aws_launch_configuration.webserver.name
-  vpc_zone_identifier  = var.subnet_ids
-
-  tag {
-    key                 = "Name"
-    value               = var.cluster_name
-    propagate_at_launch = true
-  }
-}
-
-# Default Fixed Instance (if autoscaling is disabled)
-resource "aws_instance" "webserver_instance" {
-  count         = var.enable_autoscaling ? 0 : var.min_size
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_ids[0]
-  user_data     = var.user_data
 
   tags = {
-    Name = var.cluster_name
+    Name = "terraform-example"
+  }
+}
+
+
+resource "aws_security_group" "instance" {
+  name = var.instance_name
+
+  ingress {
+
+    to_port   = var.server_port
+    from_port = var.server_port
+
   }
 }
