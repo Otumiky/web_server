8801fbbc5954cadee95f474eda2f4381124ebab5
Otu Udo <119851008+Otumiky@users.noreply.github.com>
2024-12-17 10:28:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,7 @@
-resource "aws_security_group" "webserver" {
-  name        = "${var.cluster_name}-sg"
-  description = "Allow HTTP and SSH traffic for webserver"
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
+# Reference the remote state for database configuration
 data "terraform_remote_state" "db" {
   backend = "s3"
+
   config = {
     bucket = var.db_remote_state_bucket
     key    = var.db_remote_state_key
@@ -33,40 +9,44 @@ data "terraform_remote_state" "db" {
   }
 }
 
-resource "aws_launch_template" "webserver" {
-  name_prefix   = "${var.cluster_name}-lt"
-  image_id      = var.ami
+# Launch Configuration for Webserver Instances
+resource "aws_launch_configuration" "webserver" {
+  name_prefix   = "${var.cluster_name}-lc-"
+  image_id      = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
+  user_data     = var.user_data
 
-  user_data = base64encode(templatefile("${path.module}/user-data.sh", {
-    server_text = var.server_text
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
-  }))
-
-  network_interfaces {
-    security_groups = [aws_security_group.webserver.id]
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-resource "aws_autoscaling_group" "webserver" {
-  desired_capacity     = var.min_size
-  max_size             = var.max_size
+# Auto Scaling Group for Webserver Cluster
+resource "aws_autoscaling_group" "webserver_cluster" {
+  count                = var.enable_autoscaling ? 1 : 0
+  name                 = var.cluster_name
   min_size             = var.min_size
-  launch_template      = { id = aws_launch_template.webserver.id }
+  max_size             = var.max_size
+  desired_capacity     = var.min_size
+  launch_configuration = aws_launch_configuration.webserver.name
   vpc_zone_identifier  = var.subnet_ids
-  health_check_type    = "EC2"
-  health_check_grace_period = 300
-  tags = [
-    {
-      key                 = "Name"
-      value               = var.cluster_name
-      propagate_at_launch = true
-    }
-  ]
+
+  tag {
+    key                 = "Name"
+    value               = var.cluster_name
+    propagate_at_launch = true
+  }
 }
 
-output "webserver_sg_id" {
-  value = aws_security_group.webserver.id
+# Default Fixed Instance (if autoscaling is disabled)
+resource "aws_instance" "webserver_instance" {
+  count         = var.enable_autoscaling ? 0 : var.min_size
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_ids[0]
+  user_data     = var.user_data
+
+  tags = {
+    Name = var.cluster_name
+  }
 }
